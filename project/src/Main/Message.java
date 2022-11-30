8a0cfc1ba2607349ac0e96c3f0cb9100f5370da9
Mattéo D <mattdecorsaire@gmail.com>
2022-11-30 10:29:56 +0100

    question 1 - tentative #1

--- project/src/Main/Message.java
+++ project/src/Main/Message.java
@@ -1,5 +1,16 @@
 package Main;
 
 public class Message {
-    int m = 1;
+    public int m;
+    public long id;
+
+    public Message(int i, long id) {
+        m = i;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Message " + m + " : Thread " + id;
+    }
 }
---
+++ project/src/prodcons/v1/ProdConsBuffer.java
@@ -0,0 +1,78 @@
+package prodcons.v1;
+
+import Main.IProdConsBuffer;
+import Main.Message;
+
+import java.util.concurrent.Semaphore;
+
+public class ProdConsBuffer implements IProdConsBuffer {
+    int nempty;
+    int nfull;
+    int nb_message_buffer;
+    int taille_buffer;
+    int flux_msg;
+    Semaphore fifo = new Semaphore(1);
+    Message[] buffer;
+
+    public ProdConsBuffer(int taille_buffer) {
+        this.nempty = 0;
+        this.nfull = 0;
+        this.taille_buffer = taille_buffer;
+        this.buffer = new Message[taille_buffer];
+        this.nb_message_buffer = 0;
+        this.flux_msg = 0;
+    }
+
+    /**
+     * Put the message m in the buffer
+     *
+     * @param m
+     */
+    @Override
+    public void put(Message m) throws InterruptedException {
+        fifo.acquire();
+        synchronized (this) {
+            while (nempty == 0)
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                }
+            buffer[nempty] = m;
+            nempty = nempty + 1 % taille_buffer;
+            nb_message_buffer++;
+            flux_msg++;
+        }
+        fifo.release();
+    }
+
+    /**
+     * Retrieve a message from the buffer
+     * following a FIFO order (if M1 was put berofre M2, M1
+     * is retrieved before M2)
+     */
+    @Override
+    public Message get() throws InterruptedException {
+        Message m = buffer[nfull];
+        nfull = nfull + 1 % taille_buffer;
+        nb_message_buffer--;
+        return m;
+    }
+
+    /**
+     * Returns the number of messages currently available in
+     * the buffer
+     */
+    @Override
+    public int nmsg() {
+        return nb_message_buffer;
+    }
+
+    /**
+     * Returns the total number of messages that have been put
+     * in the buffer since it's creation.
+     */
+    @Override
+    public int totmsg() {
+        return flux_msg;
+    }
+}
